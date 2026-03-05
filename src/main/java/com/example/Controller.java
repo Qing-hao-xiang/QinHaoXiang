8594200cd3d07dbb349fc0113f1059c846419f4c
Qin <57602405@qq.com>
2026-03-05 10:39:39 +0800

    更新

--- src/main/java/com/example/Controller.java
+++ src/main/java/com/example/Controller.java
@@ -1,11 +1,18 @@
 package com.example;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 @org.springframework.stereotype.Controller
 public class Controller {
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("msg", "Hello Railway!");
         return "index";
+    }
+    
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello() {
+        return "你好";
     }
 }
